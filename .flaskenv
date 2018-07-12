716111784bc34724f604e71f1337bde5c6e3086b
luca <luca.kredel@web.de>
2018-07-12 23:44:27 +0200

    Add login mechanism

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,5 @@
 FLASK_APP=app.py
 FLASK_ENV=development
 FLASK_RUN_PORT=4562
+APP_SETTINGS=./settings.cfg
+
--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, request, url_for
 app = Flask(__name__)
+app.config.from_envvar('APP_SETTINGS')
 
 @app.route('/')
-def hello_world():
+def index():
+    if not session.get('login', False):
+        return redirect(url_for('login'))
+
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['passwd'] == app.config['PASSWD']:
+            session['login'] = True
+            
+            if request.form['remember']:
+                session.permanent = True
+            
+            return redirect(url_for('index'))
+            
+    if session.get('login', False):
+        return redirect(url_for('index'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    return ''
+
