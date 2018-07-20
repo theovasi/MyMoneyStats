1a3420360cbaaa7f9c01ed658adbd30c5a730423
luca <luca.kredel@web.de>
2018-07-20 13:15:27 +0200

    Add database connection and show recently added entries as well as a form for submitting new entries on the index page

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
-from flask import Flask, render_template, session, redirect, request, url_for
+import sqlite3
+from datetime import datetime
+
+from flask import Flask, render_template, session, redirect, request, url_for, g
 app = Flask(__name__)
 app.config.from_envvar('APP_SETTINGS')
+
+DATABASE = './data/mms.sqlite'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        
+    return db
+    
+    
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    
+    if db is not None:
+        db.close()
+
+def get_rows(query, args=()):
+    cur = get_db().execute(query, args)
+    rows = cur.fetchall()
+    cur.close()
+    
+    return rows
+
 
 @app.route('/')
 def index():
     if not session.get('login', False):
         return redirect(url_for('login'))
 
-    return render_template('index.html')
+    acc_entries = get_rows('SELECT * FROM accounting_entry WHERE NOT DELETED LIMIT 10')
+
+    return render_template('index.html', acc_entries=acc_entries)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -30,4 +62,23 @@ def login():
 def logout():
     session['login'] = False
     return redirect(url_for('login'))
+    
+ 
+@app.route('/create/entry', methods=['POST'])
+def create_entry():
+	amount = request.form['amount']
+	desc = request.form['desc']
+	date = int(datetime.strptime(request.form['date'], '%Y-%m-%d').timestamp())
+	
+	cur = get_db().cursor()
+
+	print(cur.execute('select * from accounting_entry').fetchall())
+
+	query = 'INSERT INTO accounting_entry (amount, date,  desc) VALUES (?, ?, ?)'
+
+	cur.execute(query, (amount, date, desc))
+	
+	get_db().commit()
+	
+	return redirect(url_for('index'))
 
